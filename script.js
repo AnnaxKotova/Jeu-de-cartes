e37fd780172aacac04d038cc23cc224079880753
AnnaxKotova <annaxkotova@gmail.com>
2021-10-15 13:18:05 +0200

    mélange et la distribution de cartes entre 4 joueurs

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let deck = []; //le plateau de cartes
-let player1;
-let player2;
-let player3;
-let player4;
+
 
 
 
@@ -16,7 +13,7 @@ function creation_deck (name_deck){
    //organisation d'objet avec des cartes avec ses valeurs : nom, valeur, couleur
    //voici il est importante de garder le nombre (ici le clé) car on va les comparer prochainement
    for (let i = 0; i <= deck.length; i++) {
-      name_deck = Object.assign(...deck.map(k => ({ [k]: { suit: name_deck, number: k} })));
+      name_deck = Object.assign(...deck.map(k => ({ [k]: { suit: name_deck, number: k, name: k} })));
        return name_deck;
 }}
 
@@ -35,6 +32,12 @@ function named_cards(deck_suit, suit){
    delete deck_suit[13];
    deck_suit['As de ' + suit] = deck_suit[14];
    delete deck_suit[14];
+   
+   //pour leur donner le nom correspondant
+   for (let element in deck_suit) {
+      deck_suit[element]["name"] = element;
+    }
+
    return deck_suit; //pour avoir le valeur
 }
 
@@ -55,30 +58,64 @@ deck_clubs = creation_deck("trèfle");
 named_deck_clubs = named_cards(deck_clubs, "trèfle");
 
 //le plateau total
-const all_desk = Object.assign(named_deck_hearts, named_deck_spades, named_deck_diamonds, named_deck_clubs);
+let all_deck = Object.assign(named_deck_hearts, named_deck_spades, named_deck_diamonds, named_deck_clubs);
 
+//transformation au tableau, le nom est le nombre
+const deck_to_play = [].concat(...Object.values(all_deck));
 
+// affichage de toutes les cartes
+for (let i = 0; i < 51; i++){
+   console.log(deck_to_play[i]);
+}
 
-//affichage de toutes les cartes
-console.log(all_desk);
-for (let element in all_desk) {
+//ou également
+
+for (let element in all_deck) {
    console.log(element);
  }
 
- console.log(all_desk["2 de carreaux"]["suit"]);
+//mélange du plateau
+
+deck_shuffled = deck_to_play.sort(() => Math.random() - 0.5);
+
+
+console.log(deck_shuffled.length);
+
+let size = 52/4; //numéro des cards pour 1 player
+
+
+let preplay = []; //le tableu avec les cards 
+
+
+for (let i = 0; i <Math.ceil(deck_shuffled.length/size); i++){
+   preplay[i] = deck_shuffled.slice((i*size), (i*size) + size);
+}
+
+console.log(preplay);
+
+let player1 = preplay[0];
+let player2 = preplay[1];
+let player3 = preplay[2];
+let player4 = preplay[3];
+
+console.log(player1);
+console.log(player2);
+console.log(player3);
+console.log(player4);
+
 
  function compare (card_1, card_2){
-    if (card_1 > card_2){
-      console.log(card_1 +" est gagné ");
+    if (card_1["number"] > card_2["number"]){
+      console.log(card_1["name"] +" est gagné ");
       
     }
-    else if (card_2 > card_1){
-      console.log(card_2 +" est gagné ");
+    else if (card_2["number"] > card_1["number"]){
+      console.log(card_2["name"] +" est gagné ");
       
    }
    else {
-      console.log("Les valeurs des cartes sont égales");
+      console.log("Les valeurs des cartes sont égales, ex-aequo");
    }
  }
 
- console.log(compare (all_desk["2 de carreaux"]["number"], all_desk["3 de piques"]["number"]));
+compare(all_deck["2 de carreaux"], all_deck["3 de piques"]);
